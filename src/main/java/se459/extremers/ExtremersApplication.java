dfe3ac87da9f3a47657618cebeea5f7ce7ecf8e8
Jeff Wiand <42820202+bitjefe@users.noreply.github.com>
2021-10-07 12:12:00 -0500

    creates extremers Spring App and adds lombok, jpa, data-rest, thymeleaf project dependencies

---
+++ src/main/java/se459/extremers/ExtremersApplication.java
@@ -0,0 +1,13 @@
+package se459.extremers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtremersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtremersApplication.class, args);
+	}
+
+}
