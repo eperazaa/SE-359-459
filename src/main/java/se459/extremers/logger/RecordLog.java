a00bb27e41e6b6f062f8e43d9fca682da63933fc
Jeff Wiand <42820202+bitjefe@users.noreply.github.com>
2021-10-11 09:39:02 -0500

    creates RecordLogRepository mongo interface and adds logIt internal function to RecordLog class

--- src/main/java/se459/extremers/logger/RecordLog.java
+++ src/main/java/se459/extremers/logger/RecordLog.java
@@ -1,5 +1,35 @@
 package se459.extremers.logger;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "logs")
 public class RecordLog {
-    
+    @Id
+    private String id;
+    private String content;
+    private LocalDateTime createdOn;
+
+    static void logIt(String logId, String logContent, RecordLogRepository repository) {
+        Optional<RecordLog> oldLog = repository.findById(logId);
+        if (oldLog.isPresent()) {
+            repository.delete(oldLog.get());
+        }
+
+        RecordLog recordLog = new RecordLog();
+        recordLog.setId(logId);
+        recordLog.setContent(logContent);
+        recordLog.setCreatedOn(LocalDateTime.now());
+        repository.save(recordLog);
+    }
 }
---
+++ src/main/java/se459/extremers/logger/RecordLogRepository.java
@@ -0,0 +1,13 @@
+package se459.extremers.logger;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * For Pagination and Other JPA functionality beyond base CRUD services
+ * @see https://docs.spring.io/spring-data/data-jpa/docs/current/api/org/springframework/data/jpa/repository/JpaRepository.html
+ */
+
+public interface RecordLogRepository extends MongoRepository<RecordLog, String> {
+
+}
