bb610633b300b21f1555545e55a6f373e0e91219
Jeff Wiand <42820202+bitjefe@users.noreply.github.com>
2021-10-11 11:27:36 -0500

    simplifies RecordLog class and adds sample log entry. Adds @Bean to save and retrieve/print a RecordLog object to mongo in main app

--- src/main/java/se459/extremers/logger/RecordLog.java
+++ src/main/java/se459/extremers/logger/RecordLog.java
@@ -3,12 +3,11 @@ package se459.extremers.logger;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import javax.persistence.Id;
-import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Data
 @AllArgsConstructor
@@ -18,18 +17,17 @@ public class RecordLog {
     @Id
     private String id;
     private String content;
-    private LocalDateTime createdOn;
 
-    static void logIt(String logId, String logContent, RecordLogRepository repository) {
-        Optional<RecordLog> oldLog = repository.findById(logId);
-        if (oldLog.isPresent()) {
-            repository.delete(oldLog.get());
-        }
-
-        RecordLog recordLog = new RecordLog();
-        recordLog.setId(logId);
-        recordLog.setContent(logContent);
-        recordLog.setCreatedOn(LocalDateTime.now());
-        repository.save(recordLog);
-    }
+    /*
+        Sample RecordLog
+        RecordLog(
+            id=1,
+            content={
+                "action":"SWEEP",
+                "type":"ERROR",
+                "message":"Ran out of power during Sweep operation",
+                "createdOn":"2021-10-11T11:21:50.217542"
+            }
+        )
+    */
 }
--- src/main/java/se459/extremers/logger/RecordLogRepository.java
+++ src/main/java/se459/extremers/logger/RecordLogRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  * For Pagination and Other JPA functionality beyond base CRUD services
  * @see https://docs.spring.io/spring-data/data-jpa/docs/current/api/org/springframework/data/jpa/repository/JpaRepository.html
  */
-
 public interface RecordLogRepository extends MongoRepository<RecordLog, String> {
-
 }
