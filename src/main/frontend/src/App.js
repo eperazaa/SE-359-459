bfcbef3a473bc4e498f370144782950201eb769f
Jeff Wiand <42820202+bitjefe@users.noreply.github.com>
2021-11-01 13:03:17 -0500

    forms added to CreateAccount and SignIn pages

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 import CreateAccount from './components/CreateAccount';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
+            <div>
                 <CreateAccount />
-            </header>
+            </div>
         </div>
     );
 }
--- src/main/frontend/src/components/SignIn/index.js
+++ src/main/frontend/src/components/SignIn/index.js
@@ -1,7 +1,49 @@
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function SignIn() {
-    return <h1>Sign Into your Account Below</h1>;
+    return (
+        <div>
+            <Container 
+                fluid
+                style={{
+                    background:"#282c34",
+                    color: "#F6F6F6",
+                    fontSize:"calc(10px + 1.5vmin)",
+                }}>
+                <Row style={{minHeight:"100vh", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
+                    <Col xs={0} md={3}/>
+                    <Col xs={12} md={6}>
+                        <Form style={{textAlign:"left", background:"#5865F2", borderRadius:"20px", padding:80}}>
+                            <h2 class="pb-4" style={{textAlign:"center", color:"#F6F6F6"}}>
+                                Sign In
+                            </h2>
+                            <Form.Group className="my-4" controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
+                            <Form.Group className="my-4" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Enter Password" />
+                            </Form.Group>
+                            <div className="d-grid gap-2">
+                                <Button variant="outline-dark" className="mt-4" onClick={() => window.location.href = "/register-device"}>
+                                    Submit
+                                </Button>
+                                <Button variant="link" style={{color:"#F6F6F6"}} onClick={() => window.location.href = "/create-account"}>
+                                    Don't have an account? Click to sign up here!
+                                </Button>
+                            </div>
+                        </Form>
+                    </Col>
+                    <Col xs={0} md={3}/>
+                </Row>
+            </Container>
+        </div>
+    )
 }
 
 
